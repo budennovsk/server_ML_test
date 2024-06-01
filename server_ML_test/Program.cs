ec24764c7fbb2fb7d2576c43f018ecf80499731d
Python <svetloewill@mail.ru>
2024-06-01 16:51:00 +0300

    v.2

--- server_ML_test/Program.cs
+++ server_ML_test/Program.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.IO;
 using Microsoft.Data.Analysis;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms.TimeSeries;
+using System;
+using System.IO;
 
 class MainStart
 {
     static void Main(string[] args)
     {
         // Точка входа в программу
-        Microsoft.Data.Analysis.DataFrame df= GetPath();
-        Console.WriteLine(df);
+        var df = GetPath();
+        // Console.WriteLine(df);
+        ModelTrain(df);
     }
 
-    static Microsoft.Data.Analysis.DataFrame GetPath()
+
+    static (string, string) GetPath()
     {
         // Путь к каталогу, в который нужно перейти
         string targetDirectory = @"E:\Сommercial projects\C#\server_ML_test\server_ML_test\";
@@ -28,15 +35,74 @@ class MainStart
         }
 
         {
-            
         }
-      
-        var dataPath = Path.GetFullPath(@"ts2025.csv");
-        
+
+        var dataPathTrain = Path.GetFullPath(@"train.csv");
+        var dataPathTest = Path.GetFullPath(@"test.csv");
+
         // Load the data into the data frame
-        var df = DataFrame.LoadCsv(dataPath);
-        
-        return df;
-        
+        // var df = DataFrame.LoadCsv(dataPath);
+
+        // return df;
+
+        return (dataPathTrain, dataPathTest);
     }
+
+
+    static void ModelTrain((string, string) df)
+    {
+        string path_train = df.Item1;
+        string path_test = df.Item2;
+        
+        
+        
+        MLContext mlContext = new MLContext();
+        
+        IDataView data1View =
+            mlContext.Data.LoadFromTextFile<ModelInput>(path_train, separatorChar: ',', hasHeader: false);
+        IDataView data2View =
+            mlContext.Data.LoadFromTextFile<ModelInput>(path_test, separatorChar: ',', hasHeader: false);
+        
+        
+        var forecastingPipeline = mlContext.Forecasting.ForecastBySsa(
+            outputColumnName: "forecasted_count",
+            inputColumnName: "count",
+            windowSize: 7,
+            seriesLength: 12,
+            trainSize: 40,
+            horizon: 7,
+            confidenceLevel: 0.95f,
+            confidenceLowerBoundColumn: "LowerBoundRentals",
+            confidenceUpperBoundColumn: "UpperBoundRentals");
+        
+        SsaForecastingTransformer forecaster = forecastingPipeline.Fit(data1View);
+
+        var forecastEngine = forecaster.CreateTimeSeriesEngine<ModelInput, ModelOutput>(mlContext);
+    }
+    
 }
+
+
+public class ModelInput
+{
+    [LoadColumn(1)] public DateTime action_time { get; set; }
+    [LoadColumn(0)] public float count { get; set; }
+}
+
+public class TrainData
+{
+    [LoadColumn(1)] public DateTime action_time { get; set; }
+
+    [LoadColumn(0)] public float count { get; set; }
+
+
+    // Добавьте другие поля, если они присутствуют в ваших данных
+}
+
+public class ModelOutput
+{
+    public float[] forecasted_count { get; set; }
+    public float[] lower_count { get; set; }
+    public float[] upper_count { get; set; }
+}
+
